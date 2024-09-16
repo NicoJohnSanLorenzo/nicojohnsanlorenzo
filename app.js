e04c467f1b57ffc70a6a6340656cb398217094b5
nicojohn <njsanlorenzo@gbox.ncf.edu.ph>
2024-09-16 15:13:21 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', function(req, res){
+    res.send("Nico John C. San Lorenzo");
+});
+
+app.use('/api/user', authRoutes);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
